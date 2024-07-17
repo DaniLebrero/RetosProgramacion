aacb69e44b207918be97d248deb88dfa2edf8844
Daniel Lebrero <danilebrecar07@gmail.com>
2024-07-17 18:43:25 +0200

    Reto 2 conseguido

--- anagrama/reto2.java
+++ anagrama/reto2.java
@@ -4,20 +4,27 @@ import java.util.Arrays;
 public class reto2 {
 
     public static void main(String[] args) {
+
         System.out.println(findAnagram("Amor","Roma"));
+        System.out.println(findAnagram("amor", "amores")); // false
+        System.out.println(findAnagram("amor", "amor")); // false
     }
 
     private static Boolean findAnagram(String string, String string2) {
         
         if(string.toLowerCase().equals(string2.toLowerCase())){
             return false;
-        }else{
-            char[]  wordOne = string.toLowerCase().toCharArray();
-            Arrays.sort(wordOne);
-            char[]  wordTwo = string2.toLowerCase().toCharArray();
-            Arrays.sort(wordTwo);
-            return  wordOne.equals(wordTwo);
         }
+        // Convierte las palabras a arrays de caracteres
+        char[] caracteres1 = string.toLowerCase().toCharArray();
+        char[] caracteres2 = string2.toLowerCase().toCharArray();
+
+        // Ordena los arrays de caracteres
+        Arrays.sort(caracteres1);
+        Arrays.sort(caracteres2);
+
+        // Compara los arrays ordenados
+        return Arrays.equals(caracteres1, caracteres2);
         
     }
 
